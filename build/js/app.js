e7c7716c94bf480dd22dcbb91186257066338409
mstrdt <mystrdat@gmail.com>
2015-12-22 17:36:47 +0100

    fake bank for bad browsers, ios fallbacks

--- build/js/app.js
+++ build/js/app.js
@@ -4,7 +4,11 @@ var hasAudioApi = Modernizr.webaudio;
 var isAndroid = /android/i.test(ua);
 var isIos = /iphone|ipad|ipod/i.test(ua);
 var isSafari = (ua.indexOf('safari') > -1) && (ua.indexOf('chrome') == -1);
+var isBad = (isAndroid || isIos || isSafari);
 var audioElement = document.getElementById('player');
+var videoElement = document.getElementById('lights');
+var frequencyBank = {};
+var audioFrame = 1;
 
 // Santa elements
 var snowstorm = $('#snowfield');
@@ -18,12 +22,14 @@ var stacheRight = $('.mustache-right');
 var happyHolidays = $('.happy-holidays');
 var from = $('.from');
 var lights = $('#lights');
+var iosLights = $('.ios-lights');
 var playBtn = $('.droid-play');
 var notInThisBrowser = $('.not-in-this-browser');
 var reducedExperience = $('.reduced-experience');
 var salsita = $('.salsita');
 var credits = $('.credits');
 var creditsLink = $('.credits-link');
+var loading = $('.loading');
 
 // Rand range
 function rand(min, max, whole) {
@@ -31,18 +37,20 @@ function rand(min, max, whole) {
 };
 
 function setupAnimation() {
-  // Audio setup
-  window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  window.videoElement = document.getElementById('lights');
-  window.audioSrc = audioCtx.createMediaElementSource(audioElement);
-  window.analyser = audioCtx.createAnalyser();
 
-  // Bind our analyser to the media element source
-  audioSrc.connect(analyser);
-  audioSrc.connect(audioCtx.destination);
+  if (!isBad) {
+    // Audio setup
+    window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    window.audioSrc = audioCtx.createMediaElementSource(audioElement);
+    window.analyser = audioCtx.createAnalyser();
 
-  // Set amount of frequency bands
-  window.frequencyData = new Uint8Array(128);
+    // Bind our analyser to the media element source
+    audioSrc.connect(analyser);
+    audioSrc.connect(audioCtx.destination);
+
+    // Set amount of frequency bands
+    window.frequencyData = new Uint8Array(128);
+  }
 
   // States
   window.sceneStates = {
@@ -78,10 +86,20 @@ function setupAnimation() {
 function updateScene() {
   requestAnimationFrame(updateScene);
 
-  // Copy frequency data to frequencyData array
-  analyser.getByteFrequencyData(frequencyData);
+  var newFrequencyData = [];
 
-  console.log('time: ' + audioElement.currentTime);
+  if (!isBad) {
+    // Copy frequency data to frequencyData array
+    analyser.getByteFrequencyData(frequencyData);
+    newFrequencyData = frequencyData;
+  } else {
+    // Get it from preset bank
+    newFrequencyData = frequencyBank[audioFrame];
+  }
+
+  // Save to bank
+  //frequencyBank[audioFrame] = JSON.parse("[" + Array.apply([], frequencyData).join(",") + "]");
+  //audioFrame = audioFrame + 1;
 
   // Snowstorm
   if (!sceneStates.snowIn) {
@@ -102,7 +120,7 @@ function updateScene() {
   }
 
   // Left stache
-  if (frequencyData[5] > 220) {
+  if (newFrequencyData[5] > 220) {
     stacheLeft.css({ transform: 'rotate(-15deg)' });
 
     // Left hand later
@@ -118,14 +136,14 @@ function updateScene() {
   }
 
   // Right stache
-  if (frequencyData[8] > 220) {
+  if (newFrequencyData[8] > 220) {
     stacheRight.css({ transform: 'rotate(15deg)' });
   } else {
     stacheRight.css({ transform: 'rotate(0)' });
   }
 
   // Rotate hand
-  if ((audioElement.currentTime > sceneTimings.partyOn) && (frequencyData[63] > 170)) {
+  if ((audioElement.currentTime > sceneTimings.partyOn) && (newFrequencyData[63] > 170)) {
     handLeft.css({ transform: 'rotate(10deg)' });
   } else {
     handLeft.css({ transform: 'rotate(-5deg)' });
@@ -177,7 +195,7 @@ function updateScene() {
   // Text bounce
   if (audioElement.currentTime > sceneTimings.partyOnText) {
     // Rescale frequency to range
-    var scale = (((frequencyData[63] - 0) * (1.4 - 0.8)) / (255 - 0)) + 0.8;
+    var scale = (((newFrequencyData[63] - 0) * (1.4 - 0.8)) / (255 - 0)) + 0.8;
     happyHolidays.css({
       transform: 'scale(' + scale + ')',
       color: '#fff',
@@ -189,7 +207,7 @@ function updateScene() {
   // From bounce
   if (audioElement.currentTime > sceneTimings.partyOnText) {
     // Rescale frequency to range
-    var scale = (((frequencyData[82] - 0) * (1.6 - 0.8)) / (255 - 0)) + 0.8;
+    var scale = (((newFrequencyData[82] - 0) * (1.6 - 0.8)) / (255 - 0)) + 0.8;
     from.css({
       transform: 'scale(' + scale + ') rotate(' + rand(-3, 3) + 'deg)',
       transformOrigin: '50%'
@@ -199,7 +217,7 @@ function updateScene() {
   // Salsita bounce
   if (audioElement.currentTime > sceneTimings.partyOnText) {
     // Rescale frequency to range
-    var scale = (((frequencyData[112] - 0) * (1.6 - 0.8)) / (255 - 0)) + 0.8;
+    var scale = (((newFrequencyData[112] - 0) * (1.6 - 0.8)) / (255 - 0)) + 0.8;
     salsita.css({
       transform: 'scale(' + scale + ')',
       transformOrigin: '50%'
@@ -208,15 +226,33 @@ function updateScene() {
 
   // Lights
   if((!sceneStates.lights) && (audioElement.currentTime > sceneTimings.partyOn)) {
-    lights.velocity({ opacity: 0.7 }, { mobileHA: false });
+    if (!isIos) {
+      lights.velocity({ opacity: 0.7 }, { mobileHA: false });
+    } else {
+      iosLights.velocity({ opacity: 0.7 }, { mobileHA: false });
+    }
+  }
+
+  if (isBad) {
+    if (frequencyBank[audioFrame + 1]) {
+      audioFrame = audioFrame + 1;
+    } else {
+      console.log('resetting audioFrame');
+      audioFrame = 1;
+    }
   }
 }
 
 
 function runTheShow() {
+
+  // Fade loader
+  loading.removeClass('visible');
+
   playBtn.velocity({ opacity: 1 }, { duration: 1000, mobileHA: false });
   playBtn.on('click', function() {
-    // FadeOut Play controls
+    // FadeOut Play
+    playBtn.velocity({ opacity: 0 }, { duration: 1000, mobileHA: false });
     playBtn.velocity({ opacity: 0 }, { duration: 1000, mobileHA: false });
 
     audioElement.play();
@@ -239,6 +275,9 @@ function runTheShow() {
     // Run the loop
     updateScene();
     $(this).off('click');
+
+    console.log('running the show');
+
   });
 }
 
@@ -249,8 +288,15 @@ if (!hasAudioApi) {
   console.log('no audio api');
   notInThisBrowser.velocity({ left: [ '50%', '50%'], top: [ '50%', '50%'], translateX: [ '-50%', '-50%'], translateY: [ '-50%', '-50%'],  opacity: 1 }, { mobileHA: false });
 } else {
+
+  // Preload graphic
+  loading.addClass('visible');
+
+  // Load media (Hi iOS, you suck)
+  audioElement.load();
+
   // Android bug
-  if (isAndroid || isIos) {
+  if (isBad) {
     console.log('might be buggy');
     reducedExperience.velocity({ left: [ '50%', '50%'], top: [ 0, 0 ], translateX: [ '-50%', '-50%'], translateY: [ 0, [ 200, 20 ], '-100%' ],  opacity: 1 }, { mobileHA: false });
   }
@@ -260,7 +306,17 @@ if (!hasAudioApi) {
 
   // Bind audio load
   $(audioElement).on('loadeddata', function(){
-    console.log('audio ready');
-    runTheShow();
+    // Safari fake bank workaround
+    if (isBad) {
+      $.getJSON( "js/frequencyBank.json", function( data ) {
+        frequencyBank = data;
+        console.log('loaded frequencyBank');
+        console.log('audio ready');
+        runTheShow();
+      });
+    } else {
+      console.log('audio ready');
+      runTheShow();
+    }
   })
 };
